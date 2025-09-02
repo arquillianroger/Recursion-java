42a5e177829e8f2daa44e0a504b6f0d6561647f5
Manikya Sinha <153370298+arquillianroger@users.noreply.github.com>
2025-09-02 20:19:13 +0530

    Create FibonacciNumber.java

---
+++ Easy/FibonacciNumber.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int fib(int n) {
+        //Base Case
+        if(n == 0)
+        return 0;
+        if(n == 1)
+        return 1;
+
+        //Recurrence Relation
+        return fib(n - 1) + fib(n - 2);
+    }
+}

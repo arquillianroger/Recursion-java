5130bca7d7c8fd95a82fd36a98cfebfea935de26
Manikya Sinha <153370298+arquillianroger@users.noreply.github.com>
2025-09-02 23:20:44 +0530

    Update FibonacciNumber.java

--- Easy/FibonacciNumber.java
+++ Easy/FibonacciNumber.java
@@ -1,9 +1,9 @@
 class Solution {
     public int fib(int n) {
         //Base Case
-        if(n == 0)
+        if(n == 0) // term 0
         return 0;
-        if(n == 1)
+        if(n == 1) // term 1
         return 1;
 
         //Recurrence Relation
